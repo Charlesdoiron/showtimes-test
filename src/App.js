f0dec655cfb8af2b83ba0742234609ad7d48c8a1
Charlesdoiron <doironcharles@gmail.com>
2020-11-26 22:25:06 +0100

    five

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
                 <title>super {window.location.href}</title>
                 <link rel="canonical" href="http://mysite.com/example" />
                 <meta property="og:description" content={window.location.href} />
-                { window.location.href === "https://5fc01b589e5cc60009962bf8--tender-aryabhata-7440aa.netlify.app/" && <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />}
+                { window.location.href === "https://tender-aryabhata-7440aa.netlify.app" && <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />}
             </Helmet>
       <p>{window.location.href === "https://showtimes-test.vercel.app/" && <iframe title="plaf" width="560" height="315" src="https://www.youtube.com/embed/2iZeN0qvPa0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}</p>
       <p>{window.location.href === "https://showtimes-test-doironcharles.fragile.vercel.app/" && <iframe title="plouf" width="560" height="315" src="https://www.youtube.com/embed/-8j8xgkZxuE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}</p>
