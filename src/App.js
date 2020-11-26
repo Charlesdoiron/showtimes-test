22ffa856d816439fcd100011cb44de8c03767236
Charlesdoiron <doironcharles@gmail.com>
2020-11-26 22:14:50 +0100

    two

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import {Helmet} from "react-helmet";
 import './App.css';
 
@@ -11,8 +10,8 @@ function App() {
                 <title>super {window.location.href}</title>
                 <link rel="canonical" href="http://mysite.com/example" />
             </Helmet>
-      <p>{window.location.href === "https://showtimes-test.vercel.app/" && <iframe width="560" height="315" src="https://www.youtube.com/embed/2iZeN0qvPa0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}</p>
-      <p>{window.location.href === "https://showtimes-test-doironcharles.fragile.vercel.app/" && <iframe width="560" height="315" src="https://www.youtube.com/embed/-8j8xgkZxuE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}</p>
+      <p>{window.location.href === "https://showtimes-test.vercel.app/" && <iframe title="plaf' width="560" height="315" src="https://www.youtube.com/embed/2iZeN0qvPa0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}</p>
+      <p>{window.location.href === "https://showtimes-test-doironcharles.fragile.vercel.app/" && <iframe title="plouf" width="560" height="315" src="https://www.youtube.com/embed/-8j8xgkZxuE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}</p>
     </div>
   );
 }
